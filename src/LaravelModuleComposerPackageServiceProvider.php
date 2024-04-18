587ca8d9570a59182a6ed7b0a11d1912a5d0c2b0
vsotreshko <vsotreshko@gmail.com>
2024-04-18 12:28:25 +0200

    :bug: fix: Fixed routes

--- src/LaravelModuleComposerPackageServiceProvider.php
+++ src/LaravelModuleComposerPackageServiceProvider.php
@@ -3,8 +3,6 @@
 namespace Brackets\LaravelModuleComposerPackage;
 
 use Brackets\LaravelModuleComposerPackage\Commands\LaravelModuleComposerPackageCommand;
-use Brackets\LaravelModuleComposerPackage\Http\Controllers\OrderController;
-use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -19,27 +17,12 @@ class LaravelModuleComposerPackageServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('laravel-module-composer-package')
-            ->hasConfigFile()
-            ->hasViews()
+            ->hasRoutes(['craftable-pro'])
+            // ->hasConfigFile()
+            // ->hasViews()
+            // ->hasCommand(LaravelModuleComposerPackageCommand::class)
             ->hasMigrations(['create_orders_table', 'add_permissions_to_orders'])
-            ->runsMigrations()
-            ->hasCommand(LaravelModuleComposerPackageCommand::class);
-    }
+            ->runsMigrations();
 
-    public function packageRegistered()
-    {
-        Route::macro('craftablePro', function (string $baseUrl = 'admin') {
-            Route::middleware('craftable-pro-base-middlewares')->prefix($baseUrl)->group(function () {
-                Route::middleware('craftable-pro-auth-middleware')->group(function () {
-                    Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
-                    Route::get('orders/create', [OrderController::class, 'create'])->name('orders.create');
-                    Route::post('orders', [OrderController::class, 'store'])->name('orders.store');
-                    Route::get('orders/edit/{order}', [OrderController::class, 'edit'])->name('orders.edit');
-                    Route::match(['put', 'patch'], 'orders/{order}', [OrderController::class, 'update'])->name('orders.update');
-                    Route::delete('orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
-                    Route::post('orders/bulk-destroy', [OrderController::class, 'bulkDestroy'])->name('orders.bulk-destroy');
-                });
-            });
-        });
     }
 }
